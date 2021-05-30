16e06ce129f28d6cb672b0b83f8f5719763efd2a
izen57 <golovoslon@gmail.com>
2021-05-30 21:55:26 +0300

    fix

--- lab02/my_exceptions.cpp
+++ lab02/my_exceptions.cpp
@@ -2,7 +2,7 @@
 
 my_exceptions::my_exceptions(int error_line, std::string error_filename, std::string error_text)
 {
-	error_info = error_filename + ": " + std::to_string(error_line) +  ": " + error_text;
+	error_info = error_filename + ": " + std::to_string(error_line) + ": " + error_text;
 }
 
 const char* my_exceptions::what() const noexcept
